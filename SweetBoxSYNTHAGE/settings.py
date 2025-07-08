2489f7843e2772d519e498811ca67db91bb97f5d
h-el-lo <77610803+h-el-lo@users.noreply.github.com>
2025-07-08 09:17:41 +0100

    Update settings.py

--- SweetBoxSYNTHAGE/settings.py
+++ SweetBoxSYNTHAGE/settings.py
@@ -76,8 +76,14 @@ WSGI_APPLICATION = 'SweetBoxSYNTHAGE.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'ENGINE': 'django.db.backends.sqlite3',
+'ENGINE': 'django.db.backends.postgresql',
+        'NAME': '',
+'USER': '',
+'HOST': '',
+'PASSWORD': '',
+'PORT': '',
+
     }
 }
 
