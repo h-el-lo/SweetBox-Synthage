50006b02db9104ec92c7d5850dab7fa27eebf64c
h-el-lo <77610803+h-el-lo@users.noreply.github.com>
2025-07-08 08:18:08 +0100

    Update admin.py

--- midi/admin.py
+++ midi/admin.py
@@ -1,10 +1,8 @@
 from django.contrib import admin
-from .models import Preset, Knob, Joystick, ModWheel, PitchWheel, Button
+from .models import Preset, Knob, Button, Joystick
 
 # Register your models here.
 admin.site.register(Preset)
 admin.site.register(Knob)
 admin.site.register(Button)
 admin.site.register(Joystick)
-admin.site.register(ModWheel)
-admin.site.register(PitchWheel)
