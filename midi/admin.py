ed361c20a146606fc27193d1edff2188a1439ba4
h-el-lo <77610803+h-el-lo@users.noreply.github.com>
2025-07-11 22:52:45 +0100

    both themes almost done

--- midi/admin.py
+++ midi/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Preset, Knob, Button, Joystick
+from .models import Preset, Knob, Button, Joystick, Profile
 
 # Register your models here.
 admin.site.register(Preset)
 admin.site.register(Knob)
 admin.site.register(Button)
 admin.site.register(Joystick)
+admin.site.register(Profile)
