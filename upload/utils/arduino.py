dc6227e0546f504ce9375b33ee7445a37892b3d0
h-el-lo <77610803+h-el-lo@users.noreply.github.com>
2025-07-30 13:26:08 +0100

    Update arduino.py

--- upload/utils/arduino.py
+++ upload/utils/arduino.py
@@ -2,7 +2,7 @@ import subprocess
 
 def check_arduino_cli_installed():
     try:
-        output = subprocess.check_output(["arduino-cli", "version"], stderr=subprocess.STDOUT)
+        output = subprocess.check_output(["./arduino-cli", "version"], stderr=subprocess.STDOUT)
         return output.decode().strip()
     except FileNotFoundError:
         return "arduino-cli not found"
