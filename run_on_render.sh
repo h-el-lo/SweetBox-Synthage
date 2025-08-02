987bf0d9cda8935414e7ff8e14fe858bdfdde378
h-el-lo <77610803+h-el-lo@users.noreply.github.com>
2025-08-03 00:47:26 +0100

    Update run_on_render.sh

--- run_on_render.sh
+++ run_on_render.sh
@@ -1,2 +1,4 @@
 export PATH="$PWD/bin:$PATH"
-gunicorn SweetBoxSYNTHAGE.wsgi:application --bind 0.0.0.0:8000
+# uvicorn SweetBoxSYNTHAGE.wsgi:application --host 0.0.0.0 --port 8000
+# gunicorn SweetBoxSYNTHAGE.wsgi:application --bind 0.0.0.0:8000
+python manage.py runserver 0.0.0.0:8000
