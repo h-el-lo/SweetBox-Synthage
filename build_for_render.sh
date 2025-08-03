ad3d0e66e49b5ee9389b1674c2c8742488f35a7e
h-el-lo <77610803+h-el-lo@users.noreply.github.com>
2025-08-03 08:04:23 +0100

    Update build_for_render.sh

--- build_for_render.sh
+++ build_for_render.sh
@@ -19,20 +19,22 @@ export PATH="$PWD/bin:$PATH"
 ./bin/arduino-cli version
 
 ./bin/arduino-cli config add board_manager.additional_urls \
-https://raw.githubusercontent.com/espressif/arduino-esp32/gh-pages/package_esp32_index.json,\
 https://www.arduino.cc/en/packages/package_index.json,\
-http://arduino.esp8266.com/stable/package_esp8266com_index.json,\
-https://github.com/stm32duino/BoardManagerFiles/raw/main/package_stmicroelectronics_index.json
+https://espressif.github.io/arduino-esp32/package_esp32_index.json,\
+https://arduino.esp8266.com/stable/package_esp8266com_index.json,\
+https://github.com/earlephilhower/arduino-pico/releases/download/global/package_rp2040_index.json,\
+https://raw.githubusercontent.com/stm32duino/BoardManagerFiles/main/package_stmicroelectronics_index.json\
+https://raw.githubusercontent.com/sparkfun/Arduino_Boards/master/IDE_Board_Manager/package_sparkfun_index.json
 
 ./bin/arduino-cli core update-index
 
 
 
-# ./bin/arduino-cli core install arduino:avr
+./bin/arduino-cli core install arduino:avr
 # ./bin/arduino-cli core install arduino:sam
 # ./bin/arduino-cli core install arduino:samd
 ./bin/arduino-cli core install esp32:esp32
-# ./bin/arduino-cli core install STMicroelectronics:stm32
+./bin/arduino-cli core install STMicroelectronics:stm32
 
 
 
