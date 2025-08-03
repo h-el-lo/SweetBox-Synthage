6439acf005194ccd217b39c123a54cd41c5bd570
h-el-lo <77610803+h-el-lo@users.noreply.github.com>
2025-08-03 08:29:10 +0100

    Update build_for_render.sh

--- build_for_render.sh
+++ build_for_render.sh
@@ -18,13 +18,13 @@ export PATH="$PWD/bin:$PATH"
 # Optional: verify installation
 ./bin/arduino-cli version
 
-./bin/arduino-cli config add board_manager.additional_urls \
-https://www.arduino.cc/en/packages/package_index.json,\
-https://espressif.github.io/arduino-esp32/package_esp32_index.json,\
-https://arduino.esp8266.com/stable/package_esp8266com_index.json
-# https://github.com/earlephilhower/arduino-pico/releases/download/global/package_rp2040_index.json
-# https://raw.githubusercontent.com/stm32duino/BoardManagerFiles/main/package_stmicroelectronics_index.json\
-# https://raw.githubusercontent.com/sparkfun/Arduino_Boards/master/IDE_Board_Manager/package_sparkfun_index.json
+# ./bin/arduino-cli config add board_manager.additional_urls \
+# https://www.arduino.cc/en/packages/package_index.json,\
+# https://espressif.github.io/arduino-esp32/package_esp32_index.json,\
+# https://arduino.esp8266.com/stable/package_esp8266com_index.json
+# # https://github.com/earlephilhower/arduino-pico/releases/download/global/package_rp2040_index.json
+# # https://raw.githubusercontent.com/stm32duino/BoardManagerFiles/main/package_stmicroelectronics_index.json\
+# # https://raw.githubusercontent.com/sparkfun/Arduino_Boards/master/IDE_Board_Manager/package_sparkfun_index.json
 
 ./bin/arduino-cli core update-index
 
