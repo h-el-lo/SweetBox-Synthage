6fc19b5ceb10ac5e79825c3ad2f88999270ee90d
h-el-lo <77610803+h-el-lo@users.noreply.github.com>
2025-08-03 07:52:37 +0100

    Update build_for_render.sh

--- build_for_render.sh
+++ build_for_render.sh
@@ -18,8 +18,30 @@ export PATH="$PWD/bin:$PATH"
 # Optional: verify installation
 ./bin/arduino-cli version
 
+./bin/arduino-cli config add board_manager.additional_urls \
+https://raw.githubusercontent.com/espressif/arduino-esp32/gh-pages/package_esp32_index.json,\
+https://www.arduino.cc/en/packages/package_index.json,\
+http://arduino.esp8266.com/stable/package_esp8266com_index.json,\
+https://github.com/stm32duino/BoardManagerFiles/raw/main/package_stmicroelectronics_index.json
+
+./bin/arduino-cli core update-index
+
+
+
 # ./bin/arduino-cli core install arduino:avr
+# ./bin/arduino-cli core install arduino:sam
+# ./bin/arduino-cli core install arduino:samd
 ./bin/arduino-cli core install esp32:esp32
+# ./bin/arduino-cli core install STMicroelectronics:stm32
+
+
+
+./bin/arduino-cli core list
+
+./bin/arduino-cli board listall arduino:avr
+./bin/arduino-cli board listall esp32:esp32
+
+
 
 pip install --upgrade pip
 pip install -r requirements.txt
