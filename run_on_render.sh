71e8dba300c25864535eab1bd85f7b72dde99c3a
h-el-lo <77610803+h-el-lo@users.noreply.github.com>
2025-08-04 18:42:04 +0100

    Update run_on_render.sh

--- run_on_render.sh
+++ run_on_render.sh
@@ -1,5 +1,7 @@
 export PATH="$PWD/bin:$PATH"
 
+arduino-cli config set directories.data "$PWD/arduino-data"
+
 arduino-cli board listall
 
 # uvicorn SweetBoxSYNTHAGE.wsgi:application --host 0.0.0.0 --port 8000
