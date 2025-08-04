e787cf4067fa166f8002a7206538e451763523cc
h-el-lo <77610803+h-el-lo@users.noreply.github.com>
2025-08-04 17:01:38 +0100

    Update run_on_render.sh

--- run_on_render.sh
+++ run_on_render.sh
@@ -1,12 +1,10 @@
-arduino-cli board listall
-.bin/arduino-cli board listall
-
 export PATH="$PWD/bin:$PATH"
 export ARDUINO_DATA_DIR=/opt/render/.arduino15
 
+arduino-cli core install esp32:esp32
+arduino-cli core install Arduino:avr
 
 arduino-cli board listall
-.bin/arduino-cli board listall
 
 # uvicorn SweetBoxSYNTHAGE.wsgi:application --host 0.0.0.0 --port 8000
 # gunicorn SweetBoxSYNTHAGE.wsgi:application --bind 0.0.0.0:8000
